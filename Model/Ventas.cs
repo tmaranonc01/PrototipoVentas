332811b6a104f4363216c74b1fb69638bd0d42c5
Tatiana <dam08.2022@gmail.com>
2022-10-05 13:30:05 +0200

    1 er dia de desarrollo+ç

---
+++ Model/Ventas.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+
+namespace PrototipoVentas.Model
+{
+    public class Ventas : INotifyPropertyChanged
+    {
+
+        private int idVenta;
+        private DateTime fecha;
+        private double importe;
+        private string numTarjeta;
+
+        public int IdVenta
+        {
+            get
+            {
+                return idVenta;
+            }
+            set
+            {
+                idVenta = value;
+            }
+        }
+
+        public DateTime Fecha
+        {
+            get
+            {
+                return fecha;
+            }
+            set
+            {
+                fecha = value;
+            }
+        }
+
+        public double Importe
+        {
+            get
+            {
+                return importe;
+            }
+            set
+            {
+                importe = value;
+            }
+        }
+
+        public string NumTarjeta
+        {
+            get
+            {
+                return numTarjeta;
+            }
+            set
+            {
+                numTarjeta = value;
+            }
+        }
+    }
+}
---
+++ Model/articulos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+
+namespace PrototipoVentas.Model
+{
+    public class Articulos : INotifyPropertyChanged
+    {
+
+        private int idArticulo;
+        private string descripcion;
+        private double precioCoste;
+        private double iva;
+        private double pvp;
+
+        public int IdArticulo
+        {
+            get
+            {
+                return idArticulo;
+            }
+            set
+            {
+                idArticulo = value;
+            }
+        }
+        public string Descripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+            set
+            {
+                descripcion = value;
+            }
+        }
+        public double PrecioCoste
+        {
+            get
+            {
+                return precioCoste;
+            }
+            set
+            {
+                precioCoste = value;
+            }
+        }
+        public double IVA
+        {
+            get
+            {
+                return iva;
+            }
+            set
+            {
+                iva = value;
+            }
+        }
+        public double PVP
+        {
+            get
+            {
+                return pvp;
+            }
+            set
+            {
+                pvp = value;
+            }
+        }
+    }
+}
+
+ 
